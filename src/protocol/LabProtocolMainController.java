6671c3c86d372c1b080ee5da7943dd9df16eb469
rsolovye <solo1987>
2017-04-15 07:10:29 +0300

    пекdf2542

--- src/protocol/LabProtocolMainController.java
+++ src/protocol/LabProtocolMainController.java
@@ -1,48 +1,19 @@
 package protocol;
 
-import com.sun.javafx.scene.control.skin.LabeledText;
-import cust.LabeledTextField;
-import cust.ProtocolHeader;
-import javafx.collections.ListChangeListener;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.geometry.HPos;
 import javafx.geometry.Insets;
-import javafx.geometry.Pos;
-import javafx.scene.Node;
 import javafx.scene.control.*;
-import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 
 import javafx.scene.text.Font;
-import javafx.scene.text.Text;
-import net.miginfocom.layout.AC;
-import net.miginfocom.layout.LC;
 
-import org.controlsfx.control.CheckComboBox;
 import org.tbee.javafx.scene.layout.MigPane;
-import protocol.fertilization.Fertilization;
-import protocol.gamete.AltGametesController;
-import protocol.gamete.GameteSourcesController;
 import protocol.gamete.Gametes;
 import protocol.header.ProtocolHeaderController;
 import protocol.header.ProtocolHeaderPane;
-import protocol.media.MediaFooter;
-import protocol.pgd.PGD;
-import protocol.precultivation.Precultivation;
-import protocol.puncture.FolliclePunctureController;
-import protocol.signatures.CheckPoints;
-import protocol.spermiogram.Spermiogram;
 import session.ControlledScreen;
 import session.SessionController;
-
-import java.text.SimpleDateFormat;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.util.Locale;
-
-import static javafx.geometry.HPos.CENTER;
 
 /**
  * Created by bobsol on 29.03.17.
